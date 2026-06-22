e98f164e6af0ea666fda1ed5dee49d29ff316264
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-06-22 07:55:05 +0200

    initialize guiltFreeCookie project with Spring Boot and Maven setup

---
+++ src/main/java/za/ac/cput/guiltfreecookie/GuiltFreeCookieApplication.java
@@ -0,0 +1,13 @@
+package za.ac.cput.guiltfreecookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiltFreeCookieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuiltFreeCookieApplication.class, args);
+    }
+
+}
