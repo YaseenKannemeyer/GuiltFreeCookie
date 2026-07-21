b76f93bf53405df1bf2d9dc5449d1e36c6637c02
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2026-07-21 08:54:12 +0200

    fix: correct server.servlet.context-path in application.properties and update CookieFactoryTest for cleaner imports

--- src/test/java/za/ac/cput/guiltfreecookie/factory/CookieFactoryTest.java
+++ src/test/java/za/ac/cput/guiltfreecookie/factory/CookieFactoryTest.java
@@ -2,13 +2,15 @@ package za.ac.cput.guiltfreecookie.factory;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import za.ac.cput.guiltfreecookie.domain.Cookie;
+import za.ac.cput.guiltfreecookie.domain.CookieCategory;
+
 class CookieFactoryTest {
 
     @Test
-    void testCreateCookie(){
-        za.ac.cput.guiltfreecookie.domain.Cookie result = CookieFactory.createCookie("cookieId", za.ac.cput.guiltfreecookie.domain.CookieCategory.LOW_SUGAR, "description", "ingredients", "allergies", Double.valueOf(0));
+    void testCreateCookie() {
+        Cookie result = CookieFactory.createCookie("cookieId", CookieCategory.LOW_SUGAR, "description", "ingredients", "allergies", Double.valueOf(0));
         Assertions.assertEquals(null, result);
     }
 }
 
-//Generated with love by TestMe :) Please raise issues & feature requests at: https://weirddev.com/forum#!/testme
